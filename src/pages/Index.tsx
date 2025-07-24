45a2018d5443e543bdb5ac56176a0e7750000384
Moudabir <mo.moudabir@gmail.com>
2025-07-24 11:25:44 +0100

    color

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -194,7 +194,7 @@ const Index = () => {
   return (
     <div className="min-h-screen bg-background">
       {/* Header */}
-      <div className="bg-gradient-safari text-white p-4 md:p-6 shadow-safari">
+      <div className="bg-green-safari text-white p-4 md:p-6 shadow-safari">
         <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-start md:items-center space-y-4 md:space-y-0">
           <div>
             <img src={safariLogo} alt="Safari" className="h-16 md:h-20 mb-1 md:mb-2" />
